689d86abd6690053e85d23506f14d788ee78bdf9
Manju <Manju@DESKTOP-U2K6LOV>
2022-09-30 15:41:35 +0530

    Changed

---
+++ src/test/java/jenken_package/jenken_class.java
@@ -0,0 +1,13 @@
+package jenken_package;
+
+import org.testng.annotations.Test;
+
+public class jenken_class {
+	public class CreateTest {
+
+		@Test
+		public void Created() {
+			System.out.println("smoke1");
+		}
+}
+}
